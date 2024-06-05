b0836c4ce408007a39079a1cd78fca25340e584f
Andrei <andreiflausino@live.com>
2024-06-05 15:37:00 -0300

    gameboard logic done!

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -14,14 +14,35 @@ export default class GameBoard {
     ];
   }
 
+  //TODO - If the ship is bigger than the board, make it so goes upwards instead
+  //TODO - Check if a ship already exists in the cell, if it does throw an error
+  //TODO - Check if the ship is being placed on the edge/ is bigger than the board, if so, throw an error
   addShip(ship, corY, corX, direction) {
     const shipsLength = ship.length;
     let shipCounter = 0;
     for (let y = 0; y < this.board.length; y++) {
       for (let x = 0; x < this.board.length; x++) {
         if (y === corY && x === corX && shipCounter < shipsLength && direction === "vertical") {
+          if (this.board[y][x] === "S") {
+            throw "Your ship cannot be placed on top of another ship!";
+          }
+          if (shipsLength > y && shipsLength > 1) {
+            throw "Ship is vertically bigger than the board!";
+          }
           this.board[y][x] = "S";
           corY = corY + 1;
+          shipCounter += 1;
+        }
+
+        if (y === corY && x === corX && shipCounter < shipsLength && direction === "horizontal") {
+          if (this.board[y][x] === "S") {
+            throw "Your ship cannot be placed on top of another ship!";
+          }
+          if (shipsLength > x && shipsLength > 1) {
+            throw "Ship is horizontally bigger than the board!";
+          }
+          this.board[y][x] = "S";
+          corX = corX + 1;
           shipCounter += 1;
         }
       }
--- tests/GameBoard.test.js
+++ tests/GameBoard.test.js
@@ -6,11 +6,16 @@ test("Check if gameboard has a board", () => {
   expect(gameBoard.board).toBeDefined();
 });
 
-test("Check if you can add a ship to the board", () => {
+test("Check if you can add a ship vertically and horizontally to the board", () => {
   const gameBoard = new GameBoard();
-  const ship = new Ship(3, 0);
+  const shipY = new Ship(3, 0);
+  const shipX = new Ship(3, 0);
+  const shipSmall = new Ship(1, 0);
   let shipCount = 0;
-  gameBoard.addShip(ship, 4, 4, "vertical");
+
+  gameBoard.addShip(shipY, 4, 4, "vertical");
+  gameBoard.addShip(shipX, 0, 4, "horizontal");
+  gameBoard.addShip(shipSmall, 0, 0, "horizontal");
   for (let y = 0; y < gameBoard.board.length; y++) {
     for (let x = 0; x < gameBoard.board.length; x++) {
       if (gameBoard.board[y][x] === "S") {
@@ -19,5 +24,29 @@ test("Check if you can add a ship to the board", () => {
     }
   }
 
-  expect(shipCount).toBe(3);
+  expect(shipCount).toBe(7);
+});
+
+test("Is unable to add a ship on top of another ship", () => {
+  const gameBoard = new GameBoard();
+  const ship = new Ship(3, 0);
+  gameBoard.addShip(ship, 4, 4, "vertical");
+
+  expect(() => {
+    const ship1 = new Ship(3, 0);
+    gameBoard.addShip(ship1, 4, 4, "vertical");
+  }).toThrow("Your ship cannot be placed on top of another ship!");
+});
+
+test("Check if an error is thrown when adding a ship bigger than the board", () => {
+  const gameBoard = new GameBoard();
+  const ship = new Ship(30, 0);
+
+  expect(() => {
+    gameBoard.addShip(ship, 4, 4, "vertical");
+  }).toThrow("Ship is vertically bigger than the board!");
+
+  expect(() => {
+    gameBoard.addShip(ship, 4, 4, "horizontal");
+  }).toThrow("Ship is horizontally bigger than the board!");
 });
